be65ae779758154e7f574597f4878a8229183315
Milica <milicasalajic13@gmail.com>
2024-05-12 17:18:22 +0200

    dobavljanje svih proizvoda i dobavljanje proizvoda s zeljenim id-em

---
+++ web-shop/src/main/java/com/webshop/DTO/KategorijaDTO.java
@@ -0,0 +1,17 @@
+package com.webshop.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KategorijaDTO {
+    private Long id;
+    private String nazivKategorije;
+
+}
---
+++ web-shop/src/main/java/com/webshop/DTO/ProizvodDTO.java
@@ -0,0 +1,22 @@
+package com.webshop.DTO;
+
+import com.webshop.model.Kategorija;
+import com.webshop.model.TipProdaje;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProizvodDTO {
+    private Long id;
+    private String naziv;
+    private String opis;
+    private String slikaProizvoda;
+    private Kategorija kategorija;
+    private Double cena;
+    private TipProdaje tip;
+}
