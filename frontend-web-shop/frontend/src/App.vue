1b956f3ba74a53011d9a75ec58056209007286f6
Milica <milicasalajic13@gmail.com>
2024-06-04 15:58:34 +0200

    dodata navigacija u app.vue

--- frontend-web-shop/frontend/src/App.vue
+++ frontend-web-shop/frontend/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
@@ -6,8 +6,23 @@
     </nav>
     <router-view/>
   </div>
-</template>
+</template>-->
 
+<template>
+
+  <nav class="navbar">
+    <div class="search-container">
+      <input type="text" placeholder="Pretraga...">
+      <button type="submit" class="search-button">
+        <img src="@/assets/pretraga.webp" alt="Pretraga">
+      </button>
+    </div>
+    <div class="brand">
+      <span class="letter">J</span><span class="letter">O</span><span class="letter">M</span><span class="letter">I</span>
+    </div>
+  </nav>
+
+</template>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -17,16 +32,48 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 50px; /*udaljenost searcha i jomija u odnosu na velicinu navigacije*/
+  background-color: white;
+  color: black;
+}
+input[type="text"] {
+  padding: 6px;
+  border-radius: 30px; /*uokviruje pretragu*/
+  border: none; /* Uklanja okvir */
+  width: 500px; /* Postavlja širinu input polja */
+  background-color: #f0f0f0; /* Svetlo siva pozadina */
+}
+button {
+  padding: 6px 12px;
+  border-radius: 200px;
+  background-color: #f0f0f0;
+  color: white;
+  border: none;
+}
+button img {
+  width: 15px; /* Postavite željenu širinu slike */
+  height: 14px; /* Postavite željenu visinu slike */
+}
+.brand {
+  font-size: 50px; /* Postavite veličinu fonta */
+  display: flex; /* Postavite fleksibilni prikaz */
+  align-items: center; /* Centriraj elemente */
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.letter:nth-child(1) {
+  color: #773577 /* Boja slova J */
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.letter:nth-child(2) {
+  color: #4141da; /* Boja slova O */
+}
+.letter:nth-child(3) {
+  color: #ffb4f4; /* Boja slova M */
+}
+.letter:nth-child(4) {
+  color: #488871; /* Boja slova I */
 }
 </style>
--- frontend-web-shop/frontend/src/views/HomeView.vue
+++ frontend-web-shop/frontend/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Dobrodošli na Početnu Stranicu</h1>
+    <p>Ovo je primer jednostavne početne stranice.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
