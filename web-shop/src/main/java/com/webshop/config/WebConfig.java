e551be27b07a4eba9312d1e81baf99db1fa4423c
Milica <milicasalajic13@gmail.com>
2024-06-04 20:17:42 +0200

    dodata config da bi moglo da se spoji s frontendom

--- web-shop/src/main/java/com/webshop/config/WebConfig.java
+++ web-shop/src/main/java/com/webshop/config/WebConfig.java
@@ -1,2 +1,19 @@
-package com.webshop.config;public class WebConfig {
+package com.webshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://192.168.1.6:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
